40f810c4b21df0149b0a0c5079ece900a337b19f
Lalilu23 <129690967+Lalilu23@users.noreply.github.com>
2023-05-10 03:12:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let  enviar = documento . getElementById ("enviarFormulario"); 
+enviar_addEventListener ("clic" ,  funcAlert) ;
+
+function  alertafunc ( )  {
+    alert ( "Gracias. A la brevedad estaremos comunicandonos con vos" ) ;
+}
